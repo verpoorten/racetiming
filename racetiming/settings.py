eb0a48544f0a671f7ddfc16824ebb08ba5e6681b
verpoorten <leila.verpoorten@uclouvain.be>
2018-03-12 12:46:02 +0100

    Update settings.py

--- racetiming/settings.py
+++ racetiming/settings.py
@@ -14,7 +14,9 @@ import os
 from django.core.urlresolvers import reverse, reverse_lazy
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
