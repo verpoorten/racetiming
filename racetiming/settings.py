dc48a06289c0fbb36390ee0aea92256ee6052171
Hildeberto Mendonca <me@hildeberto.com>
2018-03-14 09:44:19 +0100

    Configured dj_database_url

--- racetiming/settings.py
+++ racetiming/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from django.core.urlresolvers import reverse, reverse_lazy
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
